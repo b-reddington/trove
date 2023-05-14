cae0a3f0cdb2b5f79615762ade56b5d0fed10ace
b-reddington <brandon.reddington98@gmail.com>
2023-05-14 12:50:52 -0700

    added styling to the login

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,8 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import SingleThought from './pages/SingleThought';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -45,13 +40,8 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
           <div className="container">
             <Routes>
-              <Route 
-                path="/" 
-                element={<Home />} 
-              />
               <Route 
                 path="/login" 
                 element={<Login />} 
@@ -60,20 +50,8 @@ function App() {
                 path="/signup" 
                 element={<Signup />} 
               />
-              <Route 
-                path="/me"
-                element={<Profile />}
-              />
-              <Route 
-                path="/profiles/:username" 
-                element={<Profile />}
-              />
-              <Route path="/thoughts/:thoughtId" 
-                element={<SingleThought />}
-              />
             </Routes>
           </div>
-          <Footer />
         </div>
       </Router>
     </ApolloProvider>
